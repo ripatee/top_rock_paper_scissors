56cfbde4700a74b8248940d7bf679cf1f49bfe5f
ripatee <risto.korhonen@kapsi.fi>
2022-09-11 14:15:32 +0300

    Add getComputerChoise function

--- js/game.js
+++ js/game.js
@@ -1,10 +1,30 @@
-console.log("Hello world");
+function getComputerChoice() {
+    let choice = getRandomInteger(0, 2)
+
+    if (choice == 0) {
+        return "rock";
+    }
+    else if (choice == 1) {
+        return "paper";
+    }
+    else {
+        return "scissors"
+    }
+}
+
+function getRandomInteger (min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+console.log(getComputerChoice());
 
 /*
 Pseudo coding
 
 While: Computer wins < 5 or Human wins <5
     Ask user input: Rock, paper, scissors
+        Check for legal input
+        Check capitalization
     Generate computers choice: Rock, paper or scissors
     Compare choices
     IF computer won:
