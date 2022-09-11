2d9fbc134a5612156e892fa3dfbf73418af1be75
ripatee <risto.korhonen@kapsi.fi>
2022-09-11 14:09:18 +0300

    Add pseudo coding

--- js/game.js
+++ js/game.js
@@ -1,2 +1,20 @@
 console.log("Hello world");
 
+/*
+Pseudo coding
+
+While: Computer wins < 5 or Human wins <5
+    Ask user input: Rock, paper, scissors
+    Generate computers choice: Rock, paper or scissors
+    Compare choices
+    IF computer won:
+        add win to computer
+    IF human won:
+        add win to human
+    Show results
+
+IF computer wins: 
+    Show "You lost"
+IF human wins:
+    Show "You win"
+*/
